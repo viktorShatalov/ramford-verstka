d577d286f153ac8eff65bc93f257db262d80dfed
viktr 666 <49025707+viktr1432@users.noreply.github.com>
2020-01-11 15:52:07 +0200

    start project

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// slick-slider
+$('.className').slick({
+    autoplay: false,
+    arrows: false,
+});
+// $('.product-slider_items').slick({
+//     autoplay: false,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     responsive: [
+//         {
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 1,
+//                 arrows: false,
+//             }
+//         },
+//         {
+//             breakpoint: 420,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1,
+//                 arrows: false,
+//             }
+//         },
+//         {
+//             breakpoint: 375,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1,
+//                 arrows: false,
+//             }
+//         },
+//         {
+//             breakpoint: 320,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1,
+//                 arrows: false,
+//             }
+//         }
+//     ]
+// });
+
+// menu -mobile
+// $(document).ready(function () {
+//     $('.burger-menu').click(function () {
+//         $('.burger-menu,.header-menu').toggleClass('active');
+//         $('body').toggleClass('lock');
+//     })
+// })

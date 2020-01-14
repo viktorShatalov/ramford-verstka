4228ba7019b08ba9ffbe611b28ca86d31a6fb25c
viktr 666 <49025707+viktr1432@users.noreply.github.com>
2020-01-14 20:32:16 +0200

    ++

--- js/script.js
+++ js/script.js
@@ -1,32 +1,60 @@
 // slick-slider
 $('.re-info-slider').slick({
-    autoplay: false,
+    autoplay: true,
     infinite: true,
     arrows: false,
     dots: true,
+    autoplaySpeed: 1000,
+    responsive: [
+        {
+            breakpoint: 1300,
+            settings: {
+                dots: false,
+            }
+        }
+    ]
 });
 
 $('.re-currentOffers-slider').slick({
-    autoplay: false,
+    autoplay: true,
     infinite: true,
     arrows: true,
     dots: false,
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplaySpeed: 1000,
-    // responsive: [
-    //     {
-    //       breakpoint: 1360,
-    //       settings: {
-    //         slidesToShow: 4,
-    //         slidesToScroll: 4,
-    //         infinite: true,
-    //         dots: true
-    //       }
-    //     },
-    //   ]
+    autoplaySpeed: 1500,
+    responsive: [
+        {
+          breakpoint: 1300,
+          settings: {
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+          }
+        },
+      ]
 });
 
+$('.re-newProduct-items').slick({
+    autoplay: true,
+    infinite: true,
+    arrows: false,
+    dots: false,
+    autoplaySpeed: 1800,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+        {
+          breakpoint: 1300,
+          settings: {
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        },
+      ]
+});
 
 // menu -mobile
 // $(document).ready(function () {

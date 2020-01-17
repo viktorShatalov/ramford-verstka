e80a35ea0aaf72f8f6faa4269f7bc0cf5cbef4cf
viktr 666 <49025707+viktr1432@users.noreply.github.com>
2020-01-17 15:50:08 +0200

    ++

--- js/script.js
+++ js/script.js
@@ -162,42 +162,10 @@ function closeModal(modal) {
   overlay.classList.remove('active')
 }
 
-// Matrix
-const t = [
-  'Wake up, MARCUS...\n',
-  'The Matrix has you...\n',
-  'Follow the white rabbit.',
-]
+// footer sub-menu
 
-function tepyText() {
-  let line = 0;
-  let count = 0;
-  let out = '';
-  let htmlOut = document.querySelector('.out');
-
-  function typeLine() {
-      let interval = setTimeout(function () {
-          out += t[line][count];
-          htmlOut.innerHTML = out + '|';
-          count++;
-          // Проверки
-          if (count >= t[line].length) {
-              count = 0;
-              line++;
-              if (line == t.length) {
-                  clearTimeout(interval);
-                  htmlOut.innerHTML = out;
-                  return true;
-              }
-          }
-          typeLine();
-      },getRandomInt(getRandomInt(250) * 2.5) )
-  }
-  typeLine();
-}
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-tepyText();
+$(".re-menu-column ul li").hide();
+$(".re-menu-column h3").click(
+  function(){
+  $(".re-menu-column ul li").toggle('slow')}
+);
